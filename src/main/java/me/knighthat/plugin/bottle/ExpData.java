83a343b40ef078026fe1df40f60d37d56f837863
KnightHat <knightchanelgaming@gmail.com>
2024-01-16 16:35:46 -0600

    added comment and added minor change to fix XP not enough to level up

--- src/main/java/me/knighthat/plugin/bottle/ExpData.java
+++ src/main/java/me/knighthat/plugin/bottle/ExpData.java
@@ -34,6 +34,15 @@ public class ExpData {
         if ( !isPluginBottle( meta ) )
             return -1;
 
-        return meta.getPersistentDataContainer().getOrDefault( NAMESPACED_KEY, PersistentDataType.INTEGER, -1 );
+        /*
+            "Due to how Minecraft calculates fractional XP points,
+            you may need to add 1 additional XP point to fully reach the desired level.
+
+            For example, 2920 total XP points is not enough to fully reach Level 40 so 2921 points may be required instead."
+            - DigMinecraft
+
+            For details, visit: https://www.digminecraft.com/getting_started/experience.php
+         */
+        return meta.getPersistentDataContainer().getOrDefault( NAMESPACED_KEY, PersistentDataType.INTEGER, -1 ) + 1;
     }
 }
