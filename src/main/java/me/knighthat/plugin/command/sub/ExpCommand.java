c522235e21f04ac56c2a9227330f292e1e76744e
KnightHat <knightchanelgaming@gmail.com>
2024-02-01 22:29:03 -0600

    fixed plugin allows withdrawing outside range

--- src/main/java/me/knighthat/plugin/command/sub/ExpCommand.java
+++ src/main/java/me/knighthat/plugin/command/sub/ExpCommand.java
@@ -15,12 +15,12 @@ public abstract class ExpCommand extends PlayerCommand {
     protected Player giver;
     @NotNull
     protected Player receiver;
-    @Range ( from = 0x0, to = Integer.MAX_VALUE )
-    protected int giverTotalExp;
-    @Range ( from = 0x0, to = Integer.MAX_VALUE )
-    private int withdrawAmount = -1;
-    @Range ( from = 0x0, to = Integer.MAX_VALUE )
-    private int toBottleAmount = -1;
+    @Range( from = 0x0, to = Integer.MAX_VALUE )
+    protected int    giverTotalExp;
+    @Range( from = 0x0, to = Integer.MAX_VALUE )
+    private   int    withdrawAmount = -1;
+    @Range( from = 0x0, to = Integer.MAX_VALUE )
+    private   int    toBottleAmount = -1;
 
     public ExpCommand( @NotNull ExpBottle plugin ) { super( plugin ); }
 
@@ -29,9 +29,10 @@ public abstract class ExpCommand extends PlayerCommand {
      * acceptable range stated in config.yml file.
      *
      * @param amount experience points
+     *
      * @return whether the amount is out of range
      */
-    private boolean isOutOfRange( int amount ) { return amount > plugin.config.getMax() && amount < plugin.config.getMin(); }
+    private boolean isOutOfRange( int amount ) { return amount > plugin.config.getMax() || amount < plugin.config.getMin(); }
 
     /**
      * This method does three simple jobs.
@@ -45,6 +46,7 @@ public abstract class ExpCommand extends PlayerCommand {
      *
      * @param to  who will get this bottle
      * @param exp how many xp will this bottle contain
+     *
      * @see ExperienceBottle
      */
     private void giveBottle( @NotNull Player to, int exp ) {
@@ -81,7 +83,7 @@ public abstract class ExpCommand extends PlayerCommand {
         // If not, 'toBottleAmount' stays the same as 'withdrawAmount'
         if ( plugin.config.isTaxEnabled() && !hasPermission( giver, "bypasstax" ) )
             // If tax is enabled & player doesn't have "expbottle.bypasstax" permission
-            toBottleAmount = (int) ( withdrawAmount - ( withdrawAmount * plugin.config.getTaxAmount() ) );
+            toBottleAmount = (int) (withdrawAmount - (withdrawAmount * plugin.config.getTaxAmount()));
         else
             toBottleAmount = withdrawAmount;
 
@@ -99,7 +101,7 @@ public abstract class ExpCommand extends PlayerCommand {
             plugin.messages.send( receiver, receiverMessagePath(), giver, receiver, toBottleAmount, toBottleAmount );
     }
 
-    public void setWithdrawAmount( @Range ( from = 0x0, to = Integer.MAX_VALUE ) int amount ) { this.withdrawAmount = amount; }
+    public void setWithdrawAmount( @Range( from = 0x0, to = Integer.MAX_VALUE ) int amount ) { this.withdrawAmount = amount; }
 
     @Override
     public @NotNull String getPermission() { return "user"; }
