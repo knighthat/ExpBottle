9817623b3da0496fd136387e40d73b9fe12724f2
KnightHat <knightchanelgaming@gmail.com>
2024-02-04 18:14:34 -0600

    removed unused & rearrange

--- src/main/java/me/brannstroom/expbottle/ExpBottle.java
+++ src/main/java/me/brannstroom/expbottle/ExpBottle.java
@@ -1,53 +1,27 @@
 package me.brannstroom.expbottle;
 
-import com.google.common.collect.Lists;
 import me.brannstroom.expbottle.listeners.ExpBottleListener;
 import me.knighthat.plugin.command.CommandManager;
 import me.knighthat.plugin.file.ConfigFile;
 import me.knighthat.plugin.file.MessageFile;
-import org.bukkit.Bukkit;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandMap;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class ExpBottle extends JavaPlugin {
 
-    public static ExpBottle instance;
-
     @NotNull
-    public final ConfigFile config;
+    public final ConfigFile  config;
     @NotNull
     public final MessageFile messages;
 
     public ExpBottle() {
         this.config = new ConfigFile( this );
         this.messages = new MessageFile( this );
-    }
-
-    public static void registerCommand( String name, CommandExecutor executor, String... aliases ) {
-        try {
-            Constructor<PluginCommand> constructor = PluginCommand.class.getDeclaredConstructor( String.class, Plugin.class );
-            constructor.setAccessible( true );
-
-            PluginCommand command = constructor.newInstance( name, ExpBottle.instance );
-
-            command.setExecutor( executor );
-            command.setAliases( Lists.newArrayList( aliases ) );
-            if ( executor instanceof TabCompleter ) {
-                command.setTabCompleter( (TabCompleter) executor );
-            }
-            ExpBottle.instance.getCommandMap().register( "expbottle", command );
-        } catch ( Exception e ) {
-            e.printStackTrace();
-        }
     }
 
     private void registerCommand() {
@@ -76,17 +50,5 @@ public class ExpBottle extends JavaPlugin {
 
         // Register command's executor and aliases
         registerCommand();
-    }
-
-    private CommandMap getCommandMap() {
-        try {
-            org.bukkit.Server server = Bukkit.getServer();
-            Field commandMap = server.getClass().getDeclaredField("commandMap");
-            commandMap.setAccessible(true);
-            return (CommandMap) commandMap.get(server);
-        } catch (IllegalAccessException | NoSuchFieldException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 }
